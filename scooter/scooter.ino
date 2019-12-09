cd68d7ac953c6e4240bf985177e7cc9c5c0b6cb8
Roei Dalmedigos <dalmedigos@gmail.com>
2019-12-09 17:05:00 +0200

    edit the task code and start to build the tamplate

--- scooter/scooter.ino
+++ scooter/scooter.ino
@@ -128,6 +128,36 @@ void printDebug()
   Serial.println(GyZ);
 }
 
+
+void readGyro()
+{
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true);      // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+}
+
+void canvertGyroData()
+{
+  if (GyY < 45)
+  {
+    //right
+    movetoSide(pixelsLeft);
+  }
+  else if (GyY < -45)
+  {
+    //left
+    movetoSide(pixelsRight);
+  }
+}
+
 void colorWipe(uint32_t color, int wait, Adafruit_NeoPixel pixels)
 {
   for (int i = 0; i < pixels.numPixels(); i++)
@@ -184,35 +214,6 @@ void movetoSide(Adafruit_NeoPixel strip)
     Serial.println("show");
   }
   delay(1500);
-}
-
-void readGyro()
-{
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 14, true);      // request a total of 14 registers
-  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-}
-
-void canvertGyroData()
-{
-  if (GyY < 45)
-  {
-    //right
-    movetoSide(pixelsLeft);
-  }
-  else if (GyY < -45)
-  {
-    //left
-    movetoSide(pixelsRight);
-  }
 }
 
 // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
--- tasks/tasks.ino
+++ tasks/tasks.ino
@@ -5,6 +5,7 @@
 */
 #include <Adafruit_NeoPixel.h>
 
+#define DELAYVAL 1 // Time (in milliseconds) to pause between pixels
 
 //for the yellow run led it's only index for it
 int ledrun = 0;
@@ -46,11 +47,25 @@ void artificialLoad()
 
 void codeForTask1(void *parameter)
 {
+    int pinumber = *((int *)parameter);
+    Serial.print("pin number is: ");
+    Serial.println(pinumber);
+    Adafruit_NeoPixel pixels(NUMPIXELS, pinumber, NEO_GRB + NEO_KHZ800);
+    pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 
-    Adafruit_NeoPixel pixels(NUMPIXELS, *((int *)parameter), NEO_GRB + NEO_KHZ800);
     for (;;)
     {
-        delay(1000);
+        pixels.clear();
+        delay(3000);
+        Serial.print("Finish Loop Task which runs on Core ");
+        Serial.println(xPortGetCoreID());
+
+        blinkLed(pixels);
+        movetoSide(pixels);
+        blinkLed(pixels);
+        rainbow(DELAYVAL, pixels);
+        rainbow(DELAYVAL, pixels);
+        rainbow(DELAYVAL, pixels);
     }
 }
 
@@ -78,7 +93,7 @@ void setup()
 
     xTaskCreatePinnedToCore(
         codeForTask1,
-        "led1Task",
+        "leftLed",
         1000,
         (void *)&PINpixelsLeft,
         1,
@@ -89,7 +104,7 @@ void setup()
 
     xTaskCreatePinnedToCore(
         codeForTask1,
-        "led2Task",
+        "rightLed",
         1000,
         (void *)&PINpixelsRight,
         1,
@@ -105,7 +120,7 @@ void setup()
         NULL,
         1,
         &Task3,
-        0);
+        1);
 }
 
 void loop()
@@ -119,4 +134,131 @@ void loop()
     Serial.print(" Time ");
     Serial.println(millis() - start);
     delay(10);
+}
+
+void colorWipe(uint32_t color, int wait, Adafruit_NeoPixel pixels)
+{
+    for (int i = 0; i < pixels.numPixels(); i++)
+    {                                   // For each pixel in strip...
+        pixels.setPixelColor(i, color); //  Set pixel's color (in RAM)
+        pixels.show();                  //  Update strip to match
+        delay(wait);                    //  Pause for a moment
+    }
+}
+void blinkLed(Adafruit_NeoPixel pixels)
+{
+    pixels.clear(); // Set all pixel colors to 'off'
+    for (int i = 0; i < pixels.numPixels(); i++)
+    {
+        pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+    }
+    pixels.show(); // Send the updated pixel colors to the hardware.
+    delay(100);
+    for (int i = 0; i < pixels.numPixels(); i++)
+    {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    }
+    pixels.show();
+    delay(250);
+}
+
+void movetoSide(Adafruit_NeoPixel strip)
+{
+    Serial.println("movetoside");
+
+    ledrun = 0;
+    strip.clear(); // Set all pixel colors to 'off'
+    for (int i = 0; i < (strip.numPixels() + 10) * 2; i++)
+    {
+        ledrun = i % strip.numPixels();
+        Serial.println("ledrun");
+        Serial.println(ledrun);
+
+        strip.clear(); // Set all pixel colors to 'off' or this
+        for (int j = ledrun; j < ledrun + 10; j++)
+        {
+            Serial.println("j");
+            Serial.println(j);
+            strip.setPixelColor(j, strip.Color(255, 240, 0));
+            strip.show();
+        }
+        //pixels.setPixelColor(ledrun, pixels.Color(0, 0, 0)); //withou this
+        delay(20);
+        Serial.println("show");
+    }
+    delay(1500);
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait, Adafruit_NeoPixel pixels)
+{
+    for (int a = 0; a < 10; a++)
+    { // Repeat 10 times...
+        for (int b = 0; b < 3; b++)
+        {                   //  'b' counts from 0 to 2...
+            pixels.clear(); //   Set all pixels in RAM to 0 (off)
+            // 'c' counts up from 'b' to end of strip in steps of 3...
+            for (int c = b; c < pixels.numPixels(); c += 3)
+            {
+                pixels.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+            }
+            pixels.show(); // Update strip with new contents
+            delay(wait);   // Pause for a moment
+        }
+    }
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait, Adafruit_NeoPixel pixels)
+{
+    // Hue of first pixel runs 5 complete loops through the color wheel.
+    // Color wheel has a range of 65536 but it's OK if we roll over, so
+    // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+    // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+    for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256)
+    {
+        for (int i = 0; i < pixels.numPixels(); i++)
+        { // For each pixel in strip...
+            // Offset pixel hue by an amount to make one full revolution of the
+            // color wheel (range of 65536) along the length of the strip
+            // (strip.numPixels() steps):
+            int pixelHue = firstPixelHue + (i * 65536L / pixels.numPixels());
+            // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+            // optionally add saturation and value (brightness) (each 0 to 255).
+            // Here we're using just the single-argument hue variant. The result
+            // is passed through strip.gamma32() to provide 'truer' colors
+            // before assigning to each pixel:
+            pixels.setPixelColor(i, pixels.gamma32(pixels.ColorHSV(pixelHue)));
+        }
+        pixels.show(); // Update strip with new contents
+        delay(wait);   // Pause for a moment
+    }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait, Adafruit_NeoPixel pixels)
+{
+    int firstPixelHue = 0; // First pixel starts at red (hue 0)
+    for (int a = 0; a < 30; a++)
+    { // Repeat 30 times...
+        for (int b = 0; b < 3; b++)
+        {                   //  'b' counts from 0 to 2...
+            pixels.clear(); // Set all pixel colors to 'off'
+            // 'c' counts up from 'b' to end of strip in increments of 3...
+            for (int c = b; c < pixels.numPixels(); c += 3)
+            {
+                // hue of pixel 'c' is offset by an amount to make one full
+                // revolution of the color wheel (range 65536) along the length
+                // of the strip (strip.numPixels() steps):
+                int hue = firstPixelHue + c * 65536L / pixels.numPixels();
+                uint32_t color = pixels.gamma32(pixels.ColorHSV(hue)); // hue -> RGB
+                pixels.setPixelColor(c, color);                        // Set pixel 'c' to value 'color'
+            }
+            pixels.show();               // Send the updated pixel colors to the hardware.
+            delay(wait);                 // Pause for a moment
+            firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+        }
+    }
 }
